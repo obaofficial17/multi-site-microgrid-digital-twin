6f35d8fe7993cddf0ccbca359efa11aa4b72377e
obaofficial17 <obakinemmanuel@gmail.com>
2026-07-23 11:03:41 +0100

    working

--- Rural-Telephony_Project-Frontend/supabase_config.js
+++ Rural-Telephony_Project-Frontend/supabase_config.js
@@ -1,5 +1,5 @@
 // supabase_config.js
 const SUPABASE_CONFIG = {
   URL: "https://icgryayptwjgcpqhwsxx.supabase.co",
-  ANON_KEY: "sb_publishable_U8VMbs1XABYo62cOslPnKw_PfN6rPR1"
+  ANON_KEY: "sb_publishable_U8VMbs1XABYo62cOslpNkw_PfN6rPRl"
 };
